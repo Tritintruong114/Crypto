045ca22efb086fc66e5c93ffa165877f79c62664
bruno <truongtritin.bee@gmail.com>
2023-08-15 17:27:52 +0700

    First commit UI footer & header ( responsive ) Javascript toogle hamburger menu Images export and import to the project Create brand for pages

---
+++ dest/main.js
@@ -0,0 +1,7 @@
+// Toggle Hambuger
+const toggleBurger = document.querySelector(".header__burger");
+
+toggleBurger.addEventListener("click", (e) => {
+  e.stopPropagation();
+  document.querySelector(".header__burger-line").classList.toggle("--active");
+});
