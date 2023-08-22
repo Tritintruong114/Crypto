a3a249909d9b799183f8b2716105d5ad4ef4c359
brunotruong <113487900+Tritintruong114@users.noreply.github.com>
2023-08-22 17:39:57 +0700

    this is for javascript for menu and the dropdown

--- dest/main.js
+++ dest/main.js
@@ -4,6 +4,7 @@ const toggleBurger = document.querySelector(".header__burger");
 toggleBurger.addEventListener("click", (e) => {
   e.stopPropagation();
   document.querySelector(".header__burger-line").classList.toggle("--active");
+  document.querySelector('.menu').classList.toggle('--show-menu')
 });
 
 //Slider 
@@ -11,13 +12,60 @@ toggleBurger.addEventListener("click", (e) => {
 const carousel = document.querySelector('.scuserfeedback__wrapper')
 // const flck = carousel.data('flickity')
 
-$(".scuserfeedback__wrapper").flickity({
-  cellAlign: "center",
-  prevNextButtons: true,
-  pageDots: true,
-  wrapAround: true,
-  autoPlay: 3000,
-  groupCells: 2,
-  wrapAround: true,
-})
 
+
+if (window.matchMedia("(max-width: 769px)").matches) {
+  /* the viewport is at least 400 pixels wide */
+  $(".scuserfeedback__wrapper").flickity({
+    cellAlign: "center",
+    prevNextButtons: true,
+    pageDots: true,
+    wrapAround: true,
+    autoPlay: 3000,
+    groupCells: 1,
+    wrapAround: true,
+  });
+} else {
+
+  $(".scuserfeedback__wrapper").flickity({
+    cellAlign: "center",
+    prevNextButtons: true,
+    pageDots: true,
+    wrapAround: true,
+    autoPlay: 3000,
+    groupCells: 2,
+    wrapAround: true,
+  })
+  /* the viewport is less than 400 pixels wide */
+}
+
+
+//scroll change bg header
+const headerHeight = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  let y = window.scrollY;
+  // let heightOfHero = triggerButton.offsetHeight; //Height of sc Hero
+  if (y !== 0) {
+    headerHeight.classList.add("bgblue");
+    // backToTop.classList.add("showButton");
+  } else {
+    headerHeight.classList.remove("bgblue");
+    // backToTop.classList.remove("showButton");
+  }
+});
+
+const menulist = document.querySelectorAll(".menu > li");
+
+// console.log(menulist)
+menulist.forEach((menu, index) => {
+
+  menu.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    menulist.forEach((menu, i) => {
+      menu.classList.remove('--active-menu')
+    })
+    menu.classList.add('--active-menu')
+  })
+})
