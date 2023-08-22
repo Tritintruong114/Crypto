1324cd5aab04ad5a0d3d98bad286fb0d8e08736e
brunotruong <113487900+Tritintruong114@users.noreply.github.com>
2023-08-22 09:25:29 +0700

    this is for the home carousel

--- dest/main.js
+++ dest/main.js
@@ -5,3 +5,19 @@ toggleBurger.addEventListener("click", (e) => {
   e.stopPropagation();
   document.querySelector(".header__burger-line").classList.toggle("--active");
 });
+
+//Slider 
+
+const carousel = document.querySelector('.scuserfeedback__wrapper')
+// const flck = carousel.data('flickity')
+
+$(".scuserfeedback__wrapper").flickity({
+  cellAlign: "center",
+  prevNextButtons: true,
+  pageDots: true,
+  wrapAround: true,
+  autoPlay: 3000,
+  groupCells: 2,
+  wrapAround: true,
+})
+
